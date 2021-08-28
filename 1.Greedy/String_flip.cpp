1d0bdfcf0b6e7b77d71689939557ac24485bf90c
highstar97 <pgt0185@gmail.com>
2021-08-28 19:48:23 +0900

    Solve 문자열 뒤집기

---
+++ 1.Greedy/String_flip.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str;
+
+    cin >> str;
+
+    int count = 0;
+    for(int i=0; i<str.length()-1; i++){
+        if(str[i] != str[i+1])
+            count++;
+    }
+
+    cout << (count+1)/2;
+}
